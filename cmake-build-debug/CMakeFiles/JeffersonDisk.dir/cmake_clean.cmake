fdbc25eb3f3dc75c0269f6bba651f08cc178cedb
Carmonel <zhurkovjaroslav@gmail.com>
2023-09-11 14:27:38 +0300

    Creating git repository

---
+++ cmake-build-debug/CMakeFiles/JeffersonDisk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JeffersonDisk.dir/main.cpp.o"
+  "JeffersonDisk.exe"
+  "JeffersonDisk.pdb"
+  "libJeffersonDisk.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JeffersonDisk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
