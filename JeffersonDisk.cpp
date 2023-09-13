ab55d107a66b9d1aebf89499b3f25cbef5f1780b
Carmonel <zhurkovjaroslav@gmail.com>
2023-09-14 01:25:44 +0300

    Suddenly stops at writing result section

--- JeffersonDisk.cpp
+++ JeffersonDisk.cpp
@@ -123,11 +123,81 @@ void JeffersonDisk::encodeFile(const std::string& inputPath, const std::string& 
         while (diskArray[diskNumber][readHexPosition] != readHex) readHexPosition++;
 
         // Writing shifted hex by current disk
-        std::cout << std::hex << std::uppercase << (int)diskArray[diskNumber][readHexPosition + shift] << std::endl;
-        outputFile.write(reinterpret_cast<const char *>(&diskArray[diskNumber][readHexPosition + shift]), 1);
+        std::cout << std::hex << std::uppercase << (int)diskArray[diskNumber][(readHexPosition + shift) % 256] << std::endl;
+        outputFile.write(reinterpret_cast<const char *>(&diskArray[diskNumber][(readHexPosition + shift) % 256]), 1);
         diskNumber++;
     }
 
     inputFile.close();
     outputFile.close();
+}
+
+void JeffersonDisk::decodeFile(const std::string &inputPath, const std::string &outputPath) {
+    std::ifstream inputFile;
+    try{
+        inputFile.open(inputPath, std::ios::binary | std::ios::in);
+    }
+    catch (std::exception& e){
+        std::cout << "Input file error: " << e.what() << std::endl;
+    }
+
+    std::ofstream outputFile;
+    try{
+        outputFile.open(outputPath, std::ios::binary | std::ios::out);
+    }
+    catch (std::exception& e){
+        std::cout << "Output file error: " << e.what() << std::endl;
+    }
+
+    if (!inputFile.is_open()){
+        std::cout << "Input or output file have not been opened." << std::endl;
+        exit(-1);
+    }
+    if (!outputFile.is_open()){
+        std::cout << "Output file have not been opened." << std::endl;
+        exit(-1);
+    }
+
+    // Reading size and shift
+    char readHex;
+    inputFile.read((char*)(&readHex), 1);
+    shift = readHex;
+    inputFile.read((char*)(&readHex), 1);
+    size = readHex;
+
+    std::cout << "Read shift:" << " " << shift << ", size: " << size << std::endl;
+
+    ///
+    /// Reading keys from file
+    ///
+    diskArray = new unsigned char*[size];
+    for (int i = 0; i < size; i++){
+        diskArray[i] = new unsigned char[256];
+
+        for (int j = 0; j < 256; j++){
+            inputFile.read((char*)(&readHex), 1);
+            diskArray[i][j] = readHex;
+        }
+    }
+    printDisks();
+
+    ///
+    /// Reading + writing data to file
+    ///
+    int diskNumber = 0;
+    while (inputFile.read((char*)(&readHex), 1)){
+        // Disk number loop: 0 --> size-1
+        if (diskNumber == size) diskNumber = 0;
+
+        // Finding this hex in current disk
+        int readHexPosition = 0;
+        while (diskArray[diskNumber][readHexPosition] != readHex) readHexPosition++;
+
+        // Writing shifted hex by current disk
+        int writeHexPosition = readHexPosition - shift;
+        if (writeHexPosition < 0) writeHexPosition = writeHexPosition + 256;
+        std::cout << std::hex << std::uppercase << (int)diskArray[diskNumber % size][writeHexPosition % 256] << std::endl;
+        outputFile.write(reinterpret_cast<const char *>(&diskArray[diskNumber % size][writeHexPosition % 256]), 1);
+        diskNumber++;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -6,4 +6,7 @@ int main() {
     auto* disk = new JeffersonDisk(2, 1);
     disk->printDisks();
     disk->encodeFile("C:\\Users\\Ярослав\\Documents\\in", "C:\\Users\\Ярослав\\Documents\\out");
+
+    std::cout << std::endl << std::endl << std::endl;
+    disk->decodeFile("C:\\Users\\Ярослав\\Documents\\out", "C:\\Users\\Ярослав\\Documents\\res");
 }
